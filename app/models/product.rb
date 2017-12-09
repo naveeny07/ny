12b91db67394c39f9c6ea87fdb34722cdc815edb
naveeny07 <naveeny07@gmail.com>
2017-12-09 12:13:08 +0530

    Revert "Pages update"
    This reverts commit 20736ce055cb92081012e55386bcc1adcea66892.

--- app/models/product.rb
+++
@@ -1,5 +0,0 @@
-class Product < ApplicationRecord
-validates :name, presence: true
-validates :price, presence: true, numericality: {:greater_than => 0}
-validates :short_description, presence: true
-end
--- app/models/project.rb
+++
@@ -1,5 +0,0 @@
-class Project < ApplicationRecord
-# validates :title, presence: true
-  # validates :description, presence: true
-# validates :teamsize, presence: true
-end
